fd3c3f75a58303574b7f81181f4cfc723014792c
YoshikiAbe <yoshiki.abe@jrits.ricoh.co.jp>
2016-06-06 14:37:58 +0900

    hello

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-class CommentBox extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(
-      <div className='commentBox'>
-        <h2>Comments</h2>
-        <CommentList />
-        <CommentForm />
-      </div>
-    );
-  }
-}
-
-class CommentList extends React.Component {
-  render() {
-    return(
-      <div className='commentList'>
-        hello im commentList
-      </div>
-    );
-  }
-}
-
-class CommentForm extends React.Component {
-  render() {
-    return(
-      <div className='commentForm'>
-        Hello im CommentForm
-      </div>
-    );
-  }
-}
+import CommentBox from './components/CommentBox'
 
 ReactDOM.render(
   <CommentBox />,
