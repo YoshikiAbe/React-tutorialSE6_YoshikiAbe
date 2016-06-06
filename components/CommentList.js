75ce2551da15091531e820ed5aa25fc77fd8207e
YoshikiAbe <yoshiki.abe@jrits.ricoh.co.jp>
2016-06-06 14:45:55 +0900

    iikannzi

--- components/CommentList.js
+++ components/CommentList.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Comment from './Comment';
 
 export default class CommentList extends React.Component {
   render() {
     return(
       <div className='commentList'>
-        hello im commentList
+        <Comment auther='Yoshiki'>This is one comment</Comment>
+        <Comment auther='Abe'>This is *auther* comment</Comment>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import CommentBox from './components/CommentBox'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './components/CommentBox';
 
 ReactDOM.render(
   <CommentBox />,
